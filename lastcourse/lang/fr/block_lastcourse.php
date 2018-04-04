206289c9fc2386cb1bfe79853056b429d74a5ffd
Dey Bendifallah <d.bendifallah@gmail.com>
2018-04-04 09:23:54 +0200

    Delete block_lastcourse.php

--- lastcourse/lang/fr/block_lastcourse.php
+++
@@ -1,9 +0,0 @@
-<?php
-$string['pluginname'] = 'Derni&#232res actions';
-$string['lastcourse'] = 'Derni&#232res actions';
-$string['lastcourse:addinstance'] = 'Ajouter un block lastcourse';
-$string['lastcourse_nocourse'] = 'Aucun cours consult&#233.';
-$string['lastcourse_mylastcourse'] = 'Mon dernier module.';
-$string['lastcourse_mylastmodule'] = 'Ma derni&#232re activit&#233.';
-
-?>
