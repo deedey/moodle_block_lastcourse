f134df4599f5520f8cef1943918bfdecd438988e
Dey Bendifallah <d.bendifallah@gmail.com>
2018-04-04 09:23:40 +0200

    Delete block_lastcourse.php

--- lastcourse/lang/en/block_lastcourse.php
+++
@@ -1,8 +0,0 @@
-<?php
-$string['pluginname'] = 'My last actions';
-$string['lastcourse'] = 'My last actions';
-$string['lastcourse:addinstance'] = 'Add  a block My last actions';
-$string['lastcourse_nocourse'] = 'Any course learned.';
-$string['lastcourse_mylastcourse'] = 'My last course.';
-$string['lastcourse_mylastmodule'] = 'My last module.';
-?>
