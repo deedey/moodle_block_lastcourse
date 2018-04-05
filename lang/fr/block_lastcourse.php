9e62e1edf5177ddf0e1630cbbf03c0989ab64fea
Dey Bendifallah <d.bendifallah@gmail.com>
2018-04-05 14:20:55 +0200

    Update block_lastcourse.php

--- lang/fr/block_lastcourse.php
+++ lang/fr/block_lastcourse.php
@@ -3,7 +3,6 @@ $string['pluginname'] = 'Derni&#232res actions';
 $string['lastcourse'] = 'Derni&#232res actions';
 $string['lastcourse:addinstance'] = 'Ajouter un block lastcourse';
 $string['lastcourse_nocourse'] = 'Aucun cours consult&#233.';
-$string['lastcourse_mylastcourse'] = 'Mon dernier module.';
-$string['lastcourse_mylastmodule'] = 'Ma derni&#232re activit&#233.';
-
+$string['lastcourse_mylastcourse'] = 'Mon dernier module consult&#233.';
+$string['lastcourse_mylastmodule'] = 'Ma derni&#232re activit&#233 consult&#233e.';
 ?>
