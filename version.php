d2d9c2e85cd55d20e48ac6595c63c59daa289633
Dey Bendifallah <d.bendifallah@gmail.com>
2018-04-05 14:24:40 +0200

    Update version.php

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2018021500;
-$plugin->requires  = 2017051504;
+$plugin->requires  = 2016120500;
 $plugin->component = 'block_lastcourse';
 ?>
