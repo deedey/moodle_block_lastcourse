d9f44b2c1d17f598c83f496b722bf5084785acaa
Dey Bendifallah <d.bendifallah@gmail.com>
2018-05-25 13:47:00 +0200

    Update block_lastcourse.php

--- lang/en/block_lastcourse.php
+++ lang/en/block_lastcourse.php
@@ -1,8 +1,31 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * 
+ *
+ * @package    block_lastcourse
+ * @copyright  2018 Dey Bendifallah
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'My last actions';
 $string['lastcourse'] = 'My last actions';
 $string['lastcourse:addinstance'] = 'Add  a block My last actions';
 $string['lastcourse_nocourse'] = 'Any course launched.';
 $string['lastcourse_mylastcourse'] = 'My last launched course.';
 $string['lastcourse_mylastmodule'] = 'My last launched module.';
-?>
