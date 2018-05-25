7b8a18e4a1f7a168f9d22790e0a3b17425e3d107
Dey Bendifallah <d.bendifallah@gmail.com>
2018-05-25 13:44:08 +0200

    Update access.php

--- db/access.php
+++ db/access.php
@@ -1,5 +1,29 @@
 <?php
-    $capabilities = array(
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * 
+ *
+ * @package    block_lastcourse
+ * @copyright  2018 Dey Bendifallah
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+$capabilities = array(
 
     'block/lastcourse:myaddinstance' => array(
         'captype' => 'write',
@@ -23,4 +47,3 @@
         'clonepermissionsfrom' => 'moodle/site:manageblocks'
     ),
 );
-?>
